c58cb0446e6413d9606a85d01e67fa1bde4c0064
netabirami <net.abirami@gmail.com>
2023-11-10 11:18:26 +0100

    first

---
+++ src/main/java/com/StartStep/new_student_management_v1/NewStudentManagementV1Application.java
@@ -0,0 +1,9 @@
+package com.StartStep.new_student_management_v1;
+
+
+public class NewStudentManagementV1Application {
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ src/test/java/com/StartStep/new_student_management_v1/NewStudentManagementV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StartStep.new_student_management_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewStudentManagementV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
